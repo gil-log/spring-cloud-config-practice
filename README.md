d36e9dc04ffa1ae01dc8f3620f33acf1f9204241
Gillog <swgil007@naver.com>
2023-01-09 11:50:27 +0900

    [#1]Spring Cloud Configuration Server, Client 생성

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # spring-cloud-config-practice
 Spring Cloud Configuration Server 연습
+
+# 목표
+- Spring Cloud Configuration Server는 Client에서 사용할 환경설정 파일들을 관리 할 수 있다.
+- Spring Cloud Client Server는 Configuration Server의 환경설정 파일로 구동할 수 있다.
---
+++ cloud-config/src/test/java/gil/log/cloudconfig/CloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package gil.log.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
